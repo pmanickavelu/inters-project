c9f2f34b0c01c1b06a6df493de658f2be69e6f0f
Prabhu Manickavelu <Prabhu.Manickavelu@gmail.com>
2017-03-02 13:34:48 +0530

    added code to navigate

--- test_fetch2.py
+++ test_fetch2.py
@@ -1,5 +1,5 @@
 import requests
-
+from bs4 import BeautifulSoup
 url = "http://www.amazon.in/Xbox-One-1TB-Console-Division/dp/B01F6WX6LQ"
 
 headers = {
@@ -9,3 +9,7 @@ headers = {
 response = requests.request("GET", url, headers=headers)
 
 response.text
+soup = BeautifulSoup(response.text, 'html.parser')
+print soup.find(id= "priceblock_ourprice")
+print soup.find(id="productTitle")
+print soup.find(id="wayfinding-breadcrumbs_feature_div").find_all("a", class_=["a-link-normal","a-color-tertiary"])
